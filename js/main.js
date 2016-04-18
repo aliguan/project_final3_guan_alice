dea3973fb6a3faf588009f3cb728d89c94fad6e3
Alice Guan <aliguan726@gmail.com>
2016-04-18 09:37:48 -0400

    add eventlistner to show secret menu

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ $( document ).ready( function() {
 $( document ).ready( function() {
   $( '.drawer' ).drawer();
 } );
+
+/* Show secret menu when p'ressing 'z' */
+window.addEventListener( 'keydown', revealMenu, false );
+
+var keys = [];
+
+function revealMenu( event ) {
+    keys[ event.keyCode ] = true;
+    if ( keys[ 90 ] ) {
+        var secret = document.getElementById( 'secretmenu' );
+        secret.style.display = 'block';
+    }
+}
